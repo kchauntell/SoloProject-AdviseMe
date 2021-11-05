6ed50e754b3624ed510d52357f2a08de73389c5b
Kendra Chauntell <kendrachauntell@outlook.com>
2021-11-05 15:45:18 -0400

    sign-up router completed and successfully working with validators

--- backEnd/routes/api/users.js
+++ backEnd/routes/api/users.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
+//Sign-Up
+router.post(
+  '/',
+  asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
 
-module.exports = router; 
+    await setTokenCookie(res, user);
+
+    return res.json({ user });
+  }),
+);
+
+
+module.exports = router;
