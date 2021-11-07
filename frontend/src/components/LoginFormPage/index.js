a68b8c102c2262cb52913fac0fabba8de58a0674
Kendra Chauntell <kendrachauntell@outlook.com>
2021-11-07 00:00:17 -0400

    login form complete, with small css added

---
+++ frontend/src/components/LoginFormPage/index.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch, useSelector } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import './LoginForm.css';
+
+function LoginFormPage() {
+  const dispatch = useDispatch();
+  const sessionUser = useSelector(state => state.session.user);
+  const [credential, setCredential] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState([]);
+
+  if (sessionUser) return (
+    <Redirect to="/" />
+  );
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors([]);
+    return dispatch(sessionActions.login({ credential, password }))
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <ul>
+        {errors.map((error, idx) => <li key={idx}>{error}</li>)}
+      </ul>
+      <div id='UserName-or-Email'>
+        <label>
+          Username or Email:
+          <input
+            id ='usernameoremail'
+            type="text"
+            value={credential}
+            onChange={(e) => setCredential(e.target.value)}
+            required
+          />
+        </label>
+      </div>
+      <div id='Password'>
+        <label>
+          Password: 
+          <input
+            id='passInput'
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </label>
+      </div>
+      <button id='loginButton' type="submit">Log In</button>
+    </form>
+  );
+}
+
+export default LoginFormPage;
