04b7d30077aedd7563a84841297b3536795aa037
Kendra Chauntell <kendrachauntell@outlook.com>
2021-11-08 20:01:49 -0500

    finished advice.js in models

--- backend/db/models/advice.js
+++ backend/db/models/advice.js
@@ -1,10 +1,25 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const Advice = sequelize.define('Advice', {
-    advice: DataTypes.TEXT,
-    private: DataTypes.BOOLEAN,
-    userId: DataTypes.INTEGER,
-    notesId: DataTypes.INTEGER
+    advice: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        min:10
+      }
+    },
+    private: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    notesId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
   }, {});
   Advice.associate = function(models) {
     Advice.belongsTo(models.User, {foreignKey: 'userId'});
