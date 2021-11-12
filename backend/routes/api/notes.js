000690a8055baf44b68596a3572d05da58a9deed
Kendra Chauntell <kendrachauntell@outlook.com>
2021-11-12 06:44:24 -0500

    just progress so far

--- backend/routes/api/notes.js
+++ backend/routes/api/notes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');
-const { Note } = require('../../db/models');
+const { Note, User, Notebook } = require('../../db/models');
+
+
 
 const router = express.Router();
 
@@ -9,5 +11,27 @@ router.get('/', asyncHandler(async (_req, res) => {
   return res.json(notes);
 }))
 
+router.post('/notes', asyncHandler(async (_req, res) => {
+  const {
+    note,
+    userId,
+    private,
+    noteBookId
+  } = _req.body
+
+  const user = User.findOne(id);
+  const notebook = Notebook.findOne(id);
+
+  const newNote = await Note.create({
+    note,
+    userId,
+    private,
+    noteBookId,
+    user,
+    notebook
+  })
+
+  res.redirect(`/notes/${newNote.id}`);
+}))
 
 module.exports = router;
--- frontend/src/components/CreateNotePage/index.js
+++ frontend/src/components/CreateNotePage/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useHistory } from 'react-router';
 import * as noteActions from '../../store/note';
 import { useDispatch, useSelector } from 'react-redux';
 import { Redirect } from 'react-router-dom';
@@ -6,23 +7,43 @@ import './CreateNotePage.css';
 
 function CreateNotePage() {
   const dispatch = useDispatch();
+  const history = useHistory();
   const sessionUser = useSelector(state => state.session.user);
   const [note, setNote] = useState('');
-  const [privacy, setPrivacy] = useState(false);
+  const [_private, setPrivate] = useState();
   const [errors, setErrors] = useState([]);
 
 
-  const handleSubmit = async(e) => {
+  const handleSubmit = (e) => {
+    setNote(note);
+    setPrivate(_private);
     e.preventDefault();
 
-    const payload = {
-      note,
-      privacy
+    if(!note) {
+      return dispatch(noteActions.createNote({_private, note}))
+    }
+
+    // setNote(note);
+    // setPrivate(_private);
+
+    // const payload = {
+    //   note,
+    //   _private
+    // }
+    history('/notes');
+  }
+
+  const handleChange = (e) => {
+    let checkbox = e.target;
+    if(checkbox.checked) {
+      setPrivate(e.target.value === true)
+    } else {
+      setPrivate(e.target.value === false)
     }
   }
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={(e) => handleSubmit()}>
       <ul>
         {errors.map((error, idx) => <li key={idx}>{error}</li>)}
       </ul>
@@ -37,11 +58,12 @@ function CreateNotePage() {
         </label>
       </div>
       <div>
-        <label> Would you like to keep this a secret?
-          <select>
-            <option>Yes</option>
-            <option>No</option>
-          </select>
+        <label> Would you like to keep this hidden?
+          <input
+          type='checkbox'
+          value={_private}
+          onChange={(e) => handleChange(e)}>
+          </input>
         </label>
       </div>
       <button type='submit'> Publish My Thoughts!</button>
